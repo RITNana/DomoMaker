4bb0bfdfdf77f067a0bbe3c62e9347fa46897879
RITNana <ns8363@g.rit.edu>
2024-11-12 19:45:20 -0500

    Updated-B

--- server/app.js
+++ server/app.js
@@ -6,13 +6,13 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session'); // pull in express session
 
 const router = require('./router.js');
- 
+
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || require('../mongo-connection.js');
@@ -32,6 +32,15 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// adding a section for session configuration
+app.use(session({
+  key: 'sessionid', // name of cookie
+  secret: 'Domo Arigato',
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
+// like babbel - bundling our modules :)
+
 const path = require('path');
 
 module.exports = {
-    entry: './client/client.js',
-    mode: 'production',
-    watchOptions: {
-        aggregateTimeout: 200,
-    },
-    output: {
-        path: path.resolve(__dirname, 'hosted'),
-        filename: 'bundle.js',
-    },
+  entry: './client/client.js',
+  mode: 'production',
+  watchOptions: {
+    aggregateTimeout: 200,
+  },
+  output: {
+    path: path.resolve(__dirname, 'hosted'),
+    filename: 'bundle.js',
+  },
 };
